2ef8e71b21d83b105076009b794b6e9bb1ea213b
rydesun <rydesun@gmail.com>
2017-02-10 20:56:33 +0800

    Fix global var to local

--- cdpp.sh
+++ cdpp.sh
@@ -19,17 +19,17 @@ cdpp() {
     idpath[$i]=$cpath
   done
 
-  cols=$(tput cols)
-  leftlen=0
-  idn=$(( ${#idpath[@]} - 1 ))
-  idy=0
+  local cols=$(tput cols)
+  local leftlen=0
+  local idn=$(( ${#idpath[@]} - 1 ))
+  local idy=0
 
   while (( leftlen <= ${#PWD} )); do
     echo ${PWD:$leftlen:$cols}
     leftlen=$(( leftlen + cols ))
     while (( idn >= 0 )); do
-      id=${key[idn]}
-      idx=$(( ${#idpath[$id]} - idy * cols ))
+      local id=${key[idn]}
+      local idx=$(( ${#idpath[$id]} - idy * cols ))
       (( idx <= cols && idx >= 0 )) || break
       tput hpa $idx
       printf "\e[1;31m%s\e[0m" $id
