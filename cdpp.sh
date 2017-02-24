46e3f4da920b2a911cb1ad1ba411ff3ab974ecfc
rydesun <rydesun@gmail.com>
2017-02-24 20:06:02 +0800

    Add left path loop

--- cdpp.sh
+++ cdpp.sh
@@ -8,42 +8,46 @@ cdpp() {
   #local key=({1..9})
   local key=(l k j f d s a)
 
-  local cpath=$PWD
-  declare -A idpath
+  while :; do
+    local cpath=$PWD
+    unset idpath
+    declare -A idpath
 
-  for i in ${key[@]}; do
-    cpath=${cpath%/*}
-    if [[ -z "$cpath" ]]; then
-      break
-    fi
-    idpath[$i]=$cpath
-  done
-
-  local cols=$(tput cols)
-  local leftlen=0
-  local idn=$(( ${#idpath[@]} - 1 ))
-  local idy=0
-
-  while (( leftlen <= ${#PWD} )); do
-    echo ${PWD:$leftlen:$cols}
-    leftlen=$(( leftlen + cols ))
-    while (( idn >= 0 )); do
-      local id=${key[idn]}
-      local idx=$(( ${#idpath[$id]} - idy * cols ))
-      (( idx <= cols && idx >= 0 )) || break
-      tput hpa $idx
-      printf "\e[1;31m%s\e[0m" $id
-      (( idn-- ))
+    for i in ${key[@]}; do
+      cpath=${cpath%/*}
+      if [[ -z "$cpath" ]]; then
+        break
+      fi
+      idpath[$i]=$cpath
     done
-    (( idy++ ))
-    echo
-  done
 
-  read -s -n 1 id
-  case ${!idpath[@]} in
-    *$id*)
-      cd ${idpath[$id]};;
-  esac
+    local cols=$(tput cols)
+    local leftlen=0
+    local idn=$(( ${#idpath[@]} - 1 ))
+    local idy=0
+
+    while (( leftlen <= ${#PWD} )); do
+      echo ${PWD:$leftlen:$cols}
+      leftlen=$(( leftlen + cols ))
+      while (( idn >= 0 )); do
+        local id=${key[idn]}
+        local idx=$(( ${#idpath[$id]} - idy * cols ))
+        (( idx <= cols && idx >= 0 )) || break
+        tput hpa $idx
+        printf "\e[1;31m%s\e[0m" $id
+        (( idn-- ))
+      done
+      (( idy++ ))
+      echo
+    done
+
+    read -s -n 1 id
+    case ${!idpath[@]} in
+      *$id*)
+        cd ${idpath[$id]};;
+    esac
+    [[ -n ${cpath} && ${cpath//[^\/]} != '/' ]] || break
+  done
 }
 
 alias c=cdpp
